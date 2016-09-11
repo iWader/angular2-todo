b92ab61084cbb91818c6430d395ebfd31dddcb73
Wade Urry <wade@iwader.co.uk>
2016-09-11 12:07:11 +0100

    Implement API CRUD for Todo model

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Repositories\Contracts\TodoRepositoryInterface;
+use iWader\Angular2Demo\Transformers\TodoTransformer;
+use League\Fractal\Manager;
+
+class TodoController extends Controller
+{
+    /**
+     * @var \App\Repositories\Contracts\TodoRepositoryInterface
+     */
+    protected $repository;
+
+    public function __construct(TodoRepositoryInterface $repository, Manager $fractal, Request $request)
+    {
+        parent::__construct($fractal, $request);
+
+        $this->repository = $repository;
+    }
+
+    public function index()
+    {
+        $todos = $this->repository->all();
+
+        return $this->respondWithArray($todos, new TodoTransformer());
+    }
+
+    public function create()
+    {
+        $todo = $this->repository->create($this->request->input());
+
+        return $this->respondWithItem($todo, new TodoTransformer());
+    }
+
+    public function show($id)
+    {
+        $todo = $this->repository->find($id);
+
+        return $this->respondWithItem($todo, new TodoTransformer());
+    }
+
+    public function update($id)
+    {
+        $this->repository->update($this->request->input(), $id);
+
+        $todo = $this->repository->find($id);
+
+        return $this->respondWithItem($todo, new TodoTransformer());
+    }
+
+    public function destroy($id)
+    {
+        $this->repository->delete($id);
+
+        return $this->respondWithNoContent();
+    }
+}
