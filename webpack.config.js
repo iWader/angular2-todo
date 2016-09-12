915edb5526a26d028ec874836f3b55f9863689e8
Wade Urry <wade@iwader.co.uk>
2016-09-12 20:35:40 +0100

    Initial angular setup

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+var _root = path.resolve(__dirname);
+
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [_root].concat(args));
+}
+
+module.exports = {
+    devtool: 'source-map',
+
+    entry: {
+        'polyfills': './angular/polyfills.ts',
+        'vendor': './angular/vendor.ts',
+        'app': './angular/main.ts'
+    },
+
+    output: {
+        path: root('public/angular'),
+        publicPath: './',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    htmlLoaded: {
+        minimize: false
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: root('angular'),
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                include: root('angular'),
+                loader: 'raw'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+
+        new HtmlWebpackPlugin({
+            template: 'angular/index.html'
+        })
+    ]
+};
